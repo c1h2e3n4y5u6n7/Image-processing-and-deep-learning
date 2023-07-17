4d4a6ea5456c12e2aa6fa03e54d0d6a66a74bbd0
cy <2539963132@qq.com>
2023-07-17 16:29:56 +0800

    Robets and Scharr

--- edge_detection.py
+++ edge_detection.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import imgShow as iS
+
 def SobelX(img,threshold):
     height = img.shape[0]
     width = img.shape[1]
@@ -13,6 +14,7 @@ def SobelX(img,threshold):
             if(result[i,j]<threshold):
                 result[i,j]=0
     return result
+
 def SobelY(img,threshold):
     height = img.shape[0]
     width = img.shape[1]
@@ -40,9 +42,48 @@ def Sobel(img,threshold):
             if(result[i,j]<threshold):
                 result[i,j]=0
     return result
-
-
-
+def Sobel(img,threshold):
+    height = img.shape[0]
+    width = img.shape[1]
+    G_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    G_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+    result = np.zeros(img.shape)
+    for i in range(0, width - 2):
+        for j in range(0, height - 2):
+            v = np.sum(G_x * img[i:i + 3, j:j + 3])
+            h = np.sum(G_y * img[i:i + 3, j:j + 3])
+            result[i,j] = np.sqrt((v ** 2) + (h ** 2))
+            if(result[i,j]<threshold):
+                result[i,j]=0
+    return result
+def Scharr(img,threshold):
+    height = img.shape[0]
+    width = img.shape[1]
+    G_x = np.array([[-3, 0, 3], [-10, 0, 10], [-3, 0, 3]])
+    G_y = np.array([[-3, -10, -3], [0, 0, 0], [3, 10, 3]])
+    result = np.zeros(img.shape)
+    for i in range(0, width - 2):
+        for j in range(0, height - 2):
+            v = np.sum(G_x * img[i:i + 3, j:j + 3])
+            h = np.sum(G_y * img[i:i + 3, j:j + 3])
+            result[i,j] = np.sqrt((v ** 2) + (h ** 2))
+            if(result[i,j]<threshold):
+                result[i,j]=0
+    return result
+def Roberts(img,threshold):
+    height = img.shape[0]
+    width = img.shape[1]
+    G_x = np.array([[-1, 0], [0,1]])
+    G_y = np.array([[0, -1], [1,0]])
+    result = np.zeros(img.shape)
+    for i in range(0, width - 1):
+        for j in range(0, height - 1):
+            v = np.sum(G_x * img[i:i + 2, j:j + 2])
+            h = np.sum(G_y * img[i:i + 2, j:j + 2])
+            result[i,j] = np.sqrt((v ** 2) + (h ** 2))
+            if(result[i,j]<threshold):
+                result[i,j]=0
+    return result
 def Laplacian(img):
     temLaplacian = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
     height, width = img.shape[::-1]
@@ -54,8 +95,8 @@ def Laplacian(img):
 
 img=cv2.imread("./originImg/HorizontalAndVertical.jpg")
 img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-# sobelImg=Sobel(img,56)
-# iS.showImagegray(sobelImg, img, 25, 15, 'sobelDetection', 'origin', './ProcessedImg/sobelDetection.jpg')
+sobelImg=Sobel(img,56)
+iS.showImagegray(sobelImg, img, 25, 15, 'sobelDetection', 'origin', './ProcessedImg/sobelDetection.jpg')
 imageList=[]
 origin_img=[img,'origin_img']
 imageList.append(origin_img)
@@ -73,7 +114,10 @@ img1=cv2.imread('./originImg/Goldhill.tif')
 img1=cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 LapImg=Laplacian(img1)
 iS.showImagegray(LapImg, img1, 25, 15, 'LapImg', 'origin', './ProcessedImg/lapImg.jpg')
-
+scharrImg=Scharr(img,56)
+iS.showImagegray(scharrImg, img, 25, 15, 'scharrDetection', 'origin', './ProcessedImg/scharrDetection.jpg')
+robertsImg=Roberts(img,56)
+iS.showImagegray(robertsImg, img, 25, 15, 'robertsDetection', 'origin', './ProcessedImg/robertsDetection.jpg')
 # cv2.imshow('sobely',sobely)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
